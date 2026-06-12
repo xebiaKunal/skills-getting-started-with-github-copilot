87e3825247efd1a02f5344b75a55005af67dadb7
xebiaKunal <kunal.kumar1@xebia.com>
2026-06-12 03:38:07 +0000

    added fourth activity and validation to prevent duplicate signups

--- src/app.py
+++ src/app.py
@@ -38,6 +38,12 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+    },
+    "Debate Club": {
+        "description": "Develop public speaking and argumentation skills",
+        "schedule": "Wednesdays, 4:00 PM - 5:30 PM",
+        "max_participants": 15,
+        "participants": ["alex@mergington.edu"]
     }
 }
 
